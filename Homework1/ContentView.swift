f6a96a48aa18bfaab001680e92e71a0a45e07c01
robert@pitchfords.net <5F4846B8-9F21F39D-8F5DADF2-4D0CC0A7-7D6EF7F2>
2023-12-03 14:37:19 -0800

    Initial Commit

---
+++ Homework1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework1
+//
+//  Created by Robert Pitchford on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework1/Homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework1App.swift
+//  Homework1
+//
+//  Created by Robert Pitchford on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
