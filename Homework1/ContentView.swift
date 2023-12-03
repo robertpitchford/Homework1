244e34b1b12e9cdc93600bcc92495ceec47790c2
robert@pitchfords.net <5F4846B8-9F21F39D-8F5DADF2-4D0CC0A7-7D6EF7F2>
2023-12-03 15:38:11 -0800

    Homework submission

--- Homework1/ContentView.swift
+++ Homework1/ContentView.swift
@@ -1,24 +1,46 @@
-//
-//  ContentView.swift
-//  Homework1
-//
-//  Created by Robert Pitchford on 12/3/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var redValue = 75.0
+  @State private var greenValue = 150.0
+  @State private var blueValue = 200.0
+
+  @State private var cpRed = 75.0 / 255
+  @State private var cpGreen = 150.0 / 255
+  @State private var cpBlue = 200.0 / 255
+  
+
+  var body: some View {
+    VStack {
+      Text("Color Picker")
+        .font(.title)
+      RoundedRectangle(cornerRadius: 0)
+        .fill(Color(red: cpRed, green: cpGreen, blue: cpBlue))
+      Text("Red")
+      HStack {
+        Slider(value: $redValue, in: 0.0...255.0)
+        Text("\(Int(redValue.rounded()))")
+      }
+      Text("Green")
+      HStack {
+        Slider(value: $greenValue, in: 0.0...255.0)
+        Text("\(Int(greenValue.rounded()))")
+      }
+      Text("Blue")
+      HStack {
+        Slider(value: $blueValue, in: 0.0...255.0)
+        Text("\(Int(blueValue.rounded()))")
+      }
+      Button("Set Color") {
+        cpRed = redValue / 255
+        cpGreen = greenValue / 255
+        cpBlue = blueValue / 255
+      }
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
