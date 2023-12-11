3b64db1df53515ef3cbd80cf791a3242f4fa2aab
robert@pitchfords.net <5F4846B8-9F21F39D-8F5DADF2-4D0CC0A7-7D6EF7F2>
2023-12-11 08:40:48 -0800

    Programming assignment complete

--- week2/ColorPicker/Views/ColorWellView.swift
+++ week2/ColorPicker/Views/ColorWellView.swift
@@ -32,55 +32,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State private var alertIsVisible: Bool = false
-  @State private var redColor: Double = 0.0
-  @State private var greenColor: Double = 0.0
-  @State private var blueColor: Double = 0.0
-  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
-
+struct ColorWellView: View {
+  let foregroundColor: Color
+  
   var body: some View {
-
-    VStack {
-      Text("Color Picker")
-        .font(.largeTitle)
-
-      RoundedRectangle(cornerRadius: 0)
-        .foregroundColor(foregroundColor)
-        .border(.black)
-      VStack {
-        Text("Red")
-        HStack {
-          Slider(value: $redColor, in: 0...255)
-          Text("\(Int(redColor.rounded()))")
-        }
-      }
-      VStack {
-        Text("Green")
-        HStack {
-          Slider(value: $greenColor, in: 0...255)
-          Text("\(Int(greenColor.rounded()))")
-        }
-      }
-      VStack {
-        Text("Blue")
-        HStack {
-          Slider(value: $blueColor, in: 0...255)
-          Text("\(Int(blueColor.rounded()))")
-        }
-      }
-      Button("Set Color") {
-        foregroundColor = Color(red: redColor / 255, green: greenColor / 255, blue: blueColor / 255)
-      }
-    }
-    .background(Color.white)
-    .padding(20)
-
+    RoundedRectangle(cornerRadius: Constants.colorWellCornerRadius)
+      .foregroundColor(foregroundColor)
+      .border(Color("BorderColor"), width: Constants.borderWidth)
+      .padding(.bottom, Constants.mainViewPadding)
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+#Preview {
+  ColorWellView(foregroundColor: .blue)
+    .padding()
 }
